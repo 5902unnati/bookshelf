a0c3611a767c7a363eacb85c08440822ffca6ecd
Unnati Agrawal <unnatiagrawal5902@gmail.com>
2022-08-30 10:35:02 +0530

    initial

---
+++ bookshelf/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+from bookshelf import views
+from .views import mt
+
+urlpatterns = [
+    path('',views.index,name='index'),
+    path('contacts/',views.contacts,name='contacts'),
+    path('team/',views.team,name='team'),
+    path('mt/<int:id>/',views.mt,name='mt'),
+    path('login/',views.loginpage,name='login'),
+    path('search_page/',views.search_page,name='search_page'),
+    path('logout/',views.logoutpage,name='logout'),
+    path('register/',views.register,name='register')]
